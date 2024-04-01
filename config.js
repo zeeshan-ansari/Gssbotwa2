ae0805e4a6ed3029dcb9953ee8929f8f7c083b7e
zeeshan-ansari <141929501+zeeshan-ansari@users.noreply.github.com>
2024-04-01 23:33:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,35 +1,32 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-// Website Api
-global.APIs = {
-    xfarr: 'https://api.xfarr.com',
-};
-
-// Apikey Website Api
-global.APIKeys = {
-    'https://api.xfarr.com': 'Uc3LRsLE2d',
-};
-
-// Set your URL and API key here
-global.APIs = {
-    xfarr: {
-        baseURL: 'https://api.xfarr.com',
-        Key: 'Uc3LRsLE2d'
-    }
-};
- 
 
 // Other
+global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
+global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = 'Its_krishna❤️';
-global.owner = ['5521910052631'];
-global.premium = ['5521910052631'];
-global.packname = '💓';
-global.author = 'Its_krishna❤️';
+global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
+global.owner = ['917050906659'];
+global.premium = ['917050906659'];
+global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
+global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
-global.prefa = ['', '!', '.'];
-global.sp = '✪';
+global.prefa = [".", "!", "/", "?"];
+global.typemenu = "v1"
+global.autoswview = true
+global.autoread = false
+global.available = false
+global.autoTyping = false
+global.autoRecord = false
+global.autoreact = false
+global.onlygroup = false
+global.onlypc = false
+global.welcome = true
+global.autoBlock = true
+
+/*REPLY MESSAGE*/
 global.mess = {
     banned: '`You are *Banned* fron using commands!`',
     bangc: '`This Group is *Banned* from using Commands!`',
@@ -40,36 +37,11 @@ global.mess = {
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
-    wait: '**⌛ _Ruko na thoda..._*
-*▰▰▰▱▱▱▱▱**',
+    wait: '*Processing Your request*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
-global.link = 'https://whatsapp.com/channel/0029VaFNrVHBfxoBuY2TzH2w'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
-global.autoswview = process.env.AUTO_STATUS_SEEN || "true",
-global.typemenu = 'v1' // 'v1' => 'v5'
-global.onlygroup = false // onlygroup
-global.onlypc = false
-global.AUTO_ABOUT = false
+
 global.limitawal = {
     premium: 'Infinity',
     free: 10
 };
-
-// Add anticall option
-global.config = {
-    options: {
-        antiCall: true, // Set to true for anticall functionality, false otherwise
-    },
-};
-
-// Ensure config object is defined before using it
-if (config && config.options && config.options.antiCall) {
-    let file = require.resolve(__filename);
-    fs.watchFile(file, () => {
-        fs.unwatchFile(file);
-        console.log(chalk.redBright(`Update'${__filename}'`));
-        delete require.cache[file];
-        require(file);
-    });
-}
